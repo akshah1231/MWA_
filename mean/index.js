36287a25cc760f2dc0a5fdaa8e1a5e7d23813a4f
akshah1231 <39673155+akshah1231@users.noreply.github.com>
2022-03-29 21:21:11 -0500

    second_day

---
+++ mean/index.js
@@ -0,0 +1,15 @@
+const express=require("express");
+
+const path=require("path");
+
+require("dotenv").config();
+
+const app=express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+
+
+const server=app.listen(process.env.PORT,function(){
+    console.log("Listening on port ",server.config().PORT);
+});
