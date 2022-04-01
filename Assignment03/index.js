cbc5b20d5020389d641a42b44bb110c8bfa635e0
akshah1231 <39673155+akshah1231@users.noreply.github.com>
2022-04-01 01:32:11 -0500

    assignment03 update

--- Assignment03/index.js
+++ Assignment03/index.js
@@ -9,6 +9,14 @@ const app=express();
 
 app.use("/",routes);
 
+app.use("/getsum/:firstNum",function(req,res){
+    let firstNum=parseInt(req.params.firstNum);
+    let secondNum=parseInt(req.query.secondNum);
+    let sum=firstNum+secondNum;
+    console.log("sum is "+sum);
+    res.end(sum+'');
+})
+
 const server=app.listen(process.env.PORT,function(){
     console.log("Listening on port ",server.address().port);
 });
